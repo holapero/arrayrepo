0e5990d177c8a61cfc5e6fe05a9f5d0bb230cc3b
DPL-1 <DPL-1@DPL-1s-iMac.local>
2014-02-10 11:31:56 -0800

    add methods_sample file

---
+++ methods_sample.rb
@@ -0,0 +1,11 @@
+# http://www.ruby-doc.org/core-2.1.0/String.html#method-i-split
+
+  "sentence that will be split by spaces".split(" ")
+   
+
+  # http://www.ruby-doc.org/core-2.1.0/Array.html#method-i-join
+
+  [ "a", "b", "c" ].join 
+
+  # An instance variable is assigned to a specific instance 
+  # while a local variable is held in a particular scope.
